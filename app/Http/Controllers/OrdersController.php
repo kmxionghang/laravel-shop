53bfca07d730efcbb93c34b166cc20ea1d64aa27
kmxionghang <1364052798@qq.com>
2021-04-10 15:53:59 +0800

    关闭未支付订单「未完成」

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -63,6 +64,8 @@ class OrdersController extends Controller
 
             return $order;
         });
+        // 关闭未支付订单
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
     }
