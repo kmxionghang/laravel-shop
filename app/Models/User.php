35da116c21755ebecd274738863492007b78262b
kmxionghang <1364052798@qq.com>
2021-04-10 11:03:33 +0800

    管理后台 - 用户列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,6 +11,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+    // 加上这个 Trait
+    use DefaultDatetimeFormat;
 
     /**
      * The attributes that are mass assignable.
