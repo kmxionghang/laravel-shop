c680bb228ff507cca41da9c7711144f79a940f26
kmxionghang <1364052798@qq.com>
2021-04-10 10:34:44 +0800

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,16 @@ Auth::routes();
 Auth::routes(['verify' => true]);
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
-Route::group(['middleware' => ['auth', 'verified']], function() {
+Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
 });
 
-Route::group(['middleware' => ['auth']], function (){
-   Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
+
+
+
 });
