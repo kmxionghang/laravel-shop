df4b105378d469a2f752b42a2a72bdf5c1344e7c
kmxionghang <1364052798@qq.com>
2021-04-12 09:20:16 +0800

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Admin\Controllers;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
 
 class OrdersController extends AdminController
 {
@@ -15,7 +16,7 @@ class OrdersController extends AdminController
         $grid = new Grid(new Order);
 
         // 只展示已支付的订单，并且默认按支付时间倒序排序
-        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
+//        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
 
         $grid->no('订单流水号');
         // 展示关联关系的字段时，使用 column 方法
@@ -43,5 +44,12 @@ class OrdersController extends AdminController
         });
 
         return $grid;
+    }
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => Order::find($id)]));
     }
 }
