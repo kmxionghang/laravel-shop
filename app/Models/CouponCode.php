0501d1aa25bda470624778b64e2fc3f853186e3f
kmxionghang <1364052798@qq.com>
2021-04-12 14:04:47 +0800

    用户界面 - 使用优惠券下单

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Exceptions\CouponCodeUnavailableException;
 use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -46,13 +47,13 @@ class CouponCode extends Model
         $str = '';
 
         if ($this->min_amount > 0) {
-            $str = '满'.str_replace('.00', '', $this->min_amount);
+            $str = '满' . str_replace('.00', '', $this->min_amount);
         }
         if ($this->type === self::TYPE_PERCENT) {
-            return $str.'优惠'.str_replace('.00', '', $this->value).'%';
+            return $str . '优惠' . str_replace('.00', '', $this->value) . '%';
         }
 
-        return $str.'减'.str_replace('.00', '', $this->value);
+        return $str . '减' . str_replace('.00', '', $this->value);
     }
 
     public static function findAvailableCode($length = 16)
@@ -64,5 +65,53 @@ class CouponCode extends Model
         } while (self::query()->where('code', $code)->exists());
 
         return $code;
+    }
+
+    public function checkAvailable($orderAmount = null)
+    {
+        if (!$this->enabled) {
+            throw new CouponCodeUnavailableException('优惠券不存在');
+        }
+
+        if ($this->total - $this->used <= 0) {
+            throw new CouponCodeUnavailableException('该优惠券已被兑完');
+        }
+
+        if ($this->not_before && $this->not_before->gt(Carbon::now())) {
+            throw new CouponCodeUnavailableException('该优惠券现在还不能使用');
+        }
+
+        if ($this->not_after && $this->not_after->lt(Carbon::now())) {
+            throw new CouponCodeUnavailableException('该优惠券已过期');
+        }
+
+        if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
+            throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
+        }
+    }
+
+    // 计算优惠后金额
+    public function getAdjustedPrice($orderAmount)
+    {
+        // 固定金额
+        if ($this->type === self::TYPE_FIXED) {
+            // 为了保证系统健壮性，我们需要订单金额最少为 0.01 元
+            return max(0.01, $orderAmount - $this->value);
+        }
+
+        return number_format($orderAmount * (100 - $this->value) / 100, 2, '.', '');
+    }
+
+    // 新增、减少用量
+    // 用户下单时我们新增对应优惠券的用量，如果订单超时关闭则减少用量。
+    public function changeUsed($increase = true)
+    {
+        // 传入 true 代表新增用量，否则是减少用量
+        if ($increase) {
+            // 与检查 SKU 库存类似，这里需要检查当前用量是否已经超过总量
+            return $this->where('id', $this->id)->where('used', '<', $this->total)->increment('used');
+        } else {
+            return $this->decrement('used');
+        }
     }
 }
