a3b71a2e9b2e8bacc795ead9837e236b62786993
kmxionghang <1364052798@qq.com>
2021-04-12 15:02:14 +0800

    初始化类目模型,忽略微信配置文件

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,10 +16,16 @@ class Product extends Model
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+    
     // 与商品SKU关联
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 
     public function getImageUrlAttribute()
